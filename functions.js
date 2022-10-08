aa3da2924782246edf72a528eff543f948015eb1
ms18426 <muhammad.shabbir@stud.th-deg.de>
2022-10-09 01:48:53 +0200

    update

--- functions.js
+++ functions.js
@@ -1,5 +1,14 @@
 "use strict";
+//1st way to declare the type of the function
 function add(a, b) {
     return a + b;
 }
 console.log(add(1, 2));
+// 2nd way to declare function type
+const sub = (a, b) => a - b;
+console.log(sub(5, 2));
+// 3rd way to declare function type
+const multi = function (a, b) {
+    return a * b;
+};
+console.log(multi(5, 1));
--- functions.ts
+++ functions.ts
@@ -1,4 +1,19 @@
+//1st way to declare the type of the function
+
 function add(a: number, b: number): number {
     return a + b;
 }
 console.log(add(1, 2))
+
+// 2nd way to declare function type
+
+
+const sub = (a: number, b: number): number => a - b;
+console.log(sub(5, 2))
+
+
+// 3rd way to declare function type
+const multi = function (a: number, b: number): number {
+    return a * b
+}
+console.log(multi(5, 1));
