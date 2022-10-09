69e9b56bb62af6924d4d37a825d81237afd8b261
ms18426 <muhammad.shabbir@stud.th-deg.de>
2022-10-09 14:21:03 +0200

    functions

--- functions.js
+++ functions.js
@@ -11,4 +11,20 @@ console.log(sub(5, 2));
 const multi = function (a, b) {
     return a * b;
 };
-console.log(multi(5, 1));
+console.log("The result of multiply function is", multi(5, 1));
+// optional parameter
+function sum(a, b, c) {
+    return c ? a + b + c : a + b;
+}
+console.log("The result of function with optional Parameter", sum(9, 8, 7));
+// required parameter
+function subt(a, b, c = 1) {
+    return a - b - c;
+}
+console.log(subt(9, 3, 1)); // if we dont pass the value of c/ third param it will use the default value
+//otherwise use the param value we passed
+//genric function
+function getitem(item) {
+    return new Array().concat(item);
+}
+console.log("getitems function generic function", getitem(['a', 'b', 'c', 'd']));
--- functions.ts
+++ functions.ts
@@ -16,4 +16,27 @@ console.log(sub(5, 2))
 const multi = function (a: number, b: number): number {
     return a * b
 }
-console.log(multi(5, 1));
+console.log("The result of multiply function is", multi(5, 1));
+
+// optional parameter
+function sum(a: number, b: number, c?: number) {
+    return c ? a + b + c : a + b;
+}
+console.log("The result of function with optional Parameter", sum(9, 8, 7))
+
+// required parameter
+
+function subt(a: number, b: number, c = 1) {
+    return a - b - c;
+}
+console.log(subt(9, 3, 1)); // if we dont pass the value of c/ third param it will use the default value
+//otherwise use the param value we passed
+
+
+//genric function
+function getitem<Type>(item: Type[]) {
+    return new Array<Type>().concat(item);
+
+}
+
+console.log("getitems function generic function", getitem(['a', 'b', 'c', 'd']))
